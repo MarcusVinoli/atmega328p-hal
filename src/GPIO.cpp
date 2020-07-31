379a96bbf98ffbefed4e4101228590c2546f2450
Marcus Vinicius <marcusv_3@live.com>
2020-07-31 01:14:41 -0300

    Implementa as classes de GPIO.

---
+++ src/GPIO.cpp
@@ -0,0 +1,44 @@
+//Libary for Hardware Abstraction for ATMEGA328P - GPIO Module
+//Author: Marcus V M Oliveira
+
+#include "../GPIO.h"
+
+void GPIO::writeHigh(){
+    PORT_OF_(_port) |= _bitmask;
+}
+
+void GPIO::writeLow(){
+    PORT_OF_(_port) &= ~_bitmask;
+}
+
+void GPIO::write(bool pin_level){
+    if(pin_level){
+        writeHigh();
+    } else {
+        writeLow();
+    }
+}
+
+bool GPIO::read(){
+    return((PIN_OF_(_port) & (_bitmask) != 0));
+}
+
+void GPIO::enablePullUp(){
+    writeHigh();
+}
+
+void GPIO::disablePullUp(){
+    writeLow();
+}
+
+unsigned char  GPIO::getPinRegisterValue(){
+    return(PIN_OF_(_port));
+}
+
+unsigned char  GPIO::getPortRegisterValue(){
+    return(PORT_OF_(_port));
+}
+
+unsigned char  GPIO::getDataDirectionRegisterValeu(){
+    return(DDR_OF_(_port));
+}
